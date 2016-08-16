3652b71d6da4ce9be6eacd5df34dce36722f4ffe
swiffo <svegstrup@gmail.com>
2016-08-16 21:01:05 +0100

    Bring style more in line with Google

--- main.py
+++ main.py
@@ -90,27 +90,27 @@ class SouthWind:
     """Rules for wind blowing upwards"""
     # On an irrelevant note, the south wind in Greek mythology is called Notos.
 
-    def __init__(self, windSpeed=dict()):
-        self.windSpeed = windSpeed
+    def __init__(self, wind_speed=dict()):
+        self.wind_speed = wind_speed
 
     def blow(self, position):
         """Returns the move the wind contributes"""
         x = position.coordinates()[0]
-        windSpeed = self.windSpeed.get(x, 0)
-        return Move(0, windSpeed)
+        wind_speed = self.wind_speed.get(x, 0)
+        return Move(0, wind_speed)
 
 class WestWind:
     """Rules for wind blowing to the right"""
     # The most famous of the Greek anemoi (winds), Zephyrus (or Zephyr). **APPLAUSE**
 
-    def __init__(self, windSpeed=dict()):
-        self.windSpeed = windSpeed
+    def __init__(self, wind_speed=dict()):
+        self.wind_speed = wind_speed
 
     def blow(self, position):
         """Returns the move the wind contributes"""
         y = position.coordinates()[1]
-        windSpeed = self.windSpeed.get(y, 0)
-        return Move(windSpeed, 0)
+        wind_speed = self.wind_speed.get(y, 0)
+        return Move(wind_speed, 0)
 
 ##    Finally we implement the board. We restrict to rectangular boards and a such the board/world is
 ##    determined entirely by the width, height and wind conditions. The purpose of the board is to
@@ -127,29 +127,29 @@ class Board:
         self.max_y = height - 1
         self.wind = wind
     
-    def move(self, position, agentMove):
+    def move(self, position, agent_move):
         """Move agent from specified position under the rules of the board. Returns new position"""
-        self._checkPosition(position)
+        self._check_position(position)
 
-        actualMove = agentMove + self.wind.blow(position)
+        actual_move = agent_move + self.wind.blow(position)
 
-        newPos = position.move(actualMove)
-        newPos = self._restrictToBoard(newPos)
+        new_pos = position.move(actual_move)
+        new_pos = self._restrict_to_board(new_pos)
 
-        return newPos
+        return new_pos
                            
     def dimensions(self):
         """Board dimensions (A,B): 0<=x<=A, 0<=y<=B"""
         return self.max_x, self.max_y
 
-    def _restrictToBoard(self, position):
+    def _restrict_to_board(self, position):
         """Pulls position on infinite grid to closest position on board"""
         x,y = position.coordinates()
         x = min(self.max_x, max(0, x))
         y = min(self.max_y, max(0, y))
         return Position(x,y)
 
-    def _checkPosition(self, position):
+    def _check_position(self, position):
         """Returns (x,y). Raises exception if position not on board"""
         x,y = position.coordinates()
 
@@ -158,7 +158,7 @@ class Board:
 
 ##    Lastly we implement some helpful functions.
 
-def maxArg(array):
+def max_arg(array):
     """Returns index of highest value. In case of ties, returns lowest index."""
     # I'm sure this function must be in some library somewhere but ...
     max_index = 0
@@ -171,117 +171,117 @@ def maxArg(array):
 
     return max_index
 
-def printPolicy(board, Q, moveSymbolMap, goalPosition):
+def print_policy(board, Q, move_symbol_map, goal_position):
     """Prints ASCII representation of the best move for each board position"""
     unknownMove  = "*"
-    allowedMoves = list(moveSymbolMap.keys())
+    allowed_moves = list(move_symbol_map.keys())
     max_x, max_y = board.dimensions()
 
     def moveSymbol(position):
-        if position == goalPosition:
+        if position == goal_position:
             return "@"
         
-        vals = [Q.get((position, m), -1) for m in allowedMoves]
+        vals = [Q.get((position, m), -1) for m in allowed_moves]
         
         if max(vals) == -1:
             return unknownMove
         else:
-            return moveSymbolMap[allowedMoves[maxArg(vals)]]
+            return move_symbol_map[allowed_moves[max_arg(vals)]]
             
-    symbols = [[moveSymbol(Position(x,y)) for x in range(max_x+1)] for y in range(max_y, -1, -1)]
+    symbols = [[moveSymbol(Position(x, y)) for x in range(max_x+1)] for y in range(max_y, -1, -1)]
     text = "\n".join([" ".join(line) for line in symbols])
     print(text)
 
-def defaultActionValue():
+def default_action_value():
     """Initial estimated value of state-action pair"""
     # This is turned into a function to allow for other ways of generating default estimates.
     # E.g., random numbers. The value should be considered relative to the win-reward (100).
     return 1
 
-def generateStrategy(board,
-                     startPos,
-                     goalPos,
-                     totalSteps=25000, # Number of steps (whether we reach the goal or not) involved in the learning
+def generate_strategy(board,
+                     start_pos,
+                     goal_pos,
+                     total_steps=25000, # Number of steps (whether we reach the goal or not) involved in the learning
                      alpha=0.05,       # The learning speed
                      gamma=0.9,        # Time value of future rewards (affects how much we appreciate speedy solutions)
                      epsilon=0.05,     # The epsilon of the epsilon-greedy strategy (how often we explore randomly)
-                     moveSymbolMap = {Move(-1,0):"L", Move(1,0):"R", Move(0,1):"U", Move(0,-1):"D"}
+                     move_symbol_map = {Move(-1,0):"L", Move(1,0):"R", Move(0,1):"U", Move(0,-1):"D"}
                      ):
     """Run temporal difference method and print final best actions"""
 
     # Create the moves the agent can makes as well as their ASCII representations
-    allowedMoves  = list(moveSymbolMap.keys())
+    allowed_moves  = list(move_symbol_map.keys())
 
     Q = dict() # The state-action (estimated) values. Entry keys are (pos, move).
     
     print("Starting wild wandering ...")
-    pos = startPos.clone()
-    for _step in range(totalSteps):
+    pos = start_pos.clone()
+    for _step in range(total_steps):
         if random.random() < epsilon:
             # With chance epsilon we do a random exploratory move
-            chosenMove = random.choice(allowedMoves) 
+            chosen_move = random.choice(allowed_moves) 
         else:
             # Otherwise we choose the estimated optimal move
 
             # Find the best move
-            actionVals = [Q.setdefault((pos, m), defaultActionValue()) for m in allowedMoves]
-            chosenMove = allowedMoves[maxArg(actionVals)]
+            action_values = [Q.setdefault((pos, m), default_action_value()) for m in allowed_moves]
+            chosen_move = allowed_moves[max_arg(action_values)]
 
             # Estimate the value of the position we arrive at. It's the value of the state-action pair chosen under
             # our epsilon-greedy strategy.
-            newPos = board.move(pos, chosenMove)
+            new_pos = board.move(pos, chosen_move)
             if random.random() < epsilon:
-                newPosMove = random.choice(allowedMoves)
+                new_pos_move = random.choice(allowed_moves)
             else:
-                newPosMove = allowedMoves[maxArg([Q.setdefault((newPos, m), defaultActionValue()) for m in allowedMoves])]
+                new_pos_move = allowed_moves[max_arg([Q.setdefault((new_pos, m), default_action_value()) for m in allowed_moves])]
                 
-            newPosVal = gamma * Q.setdefault((newPos, newPosMove), defaultActionValue())
+            new_pos_val = gamma * Q.setdefault((new_pos, new_pos_move), default_action_value())
 
-            # Adjust the value of the (current position, chosenMove) pair.
-            valueIncrement = alpha * (newPosVal - Q[(pos,chosenMove)])
-            if newPos == goalPos:
-                valueIncrement += alpha * 100 # If we have arrived at the goal, that's worth 100!!
-            Q[(pos,chosenMove)] += valueIncrement
+            # Adjust the value of the (current position, chosen_move) pair.
+            value_increment = alpha * (new_pos_val - Q[(pos, chosen_move)])
+            if new_pos == goal_pos:
+                value_increment += alpha * 100 # If we have arrived at the goal, that's worth 100!!
+            Q[(pos, chosen_move)] += value_increment
 
             # If we arrived at the goal, end the episode and start over.
-            if newPos == goalPos:
-                pos = startPos
+            if new_pos == goal_pos:
+                pos = start_pos
             else:
-                pos = newPos
+                pos = new_pos
 
-    print("Exhausted after {} steps... stopping.\n".format(totalSteps))
-    printPolicy(board, Q, moveSymbolMap, goalPos)
+    print("Exhausted after {} steps... stopping.\n".format(total_steps))
+    print_policy(board, Q, move_symbol_map, goal_pos)
 
 
 ##    Some examples of the learning algorithm in action:
 
 def example1():
     """Example of 10x7 board with a wind from the south"""
-    windSpeeds = {3:1, 4:1, 5:1, 6:2, 7:2, 8:1} 
-    wind = SouthWind(windSpeeds)
+    wind_speeds = {3:1, 4:1, 5:1, 6:2, 7:2, 8:1} 
+    wind = SouthWind(wind_speeds)
     board = Board(10, 7, wind)
-    startPos = Position(1, 3)
-    goalPos = Position(7,3)
+    start_pos = Position(1, 3)
+    goal_pos = Position(7,3)
 
-    generateStrategy(board, startPos, goalPos)
+    generate_strategy(board, start_pos, goal_pos)
 
 def example2():
     """Example of an 'express-way' wind. Strong wind to the right (east) on a single y-level"""
     wind  = WestWind({1:4})
     board = Board(12, 7, wind)
 
-    startingPosition = Position(1,3)
-    goalPosition     = Position(10, 3)
+    starting_position = Position(1,3)
+    goal_position     = Position(10, 3)
 
-    generateStrategy(board, startingPosition, goalPosition, totalSteps=50000)
+    generate_strategy(board, starting_position, goal_position, total_steps=50000)
 
 def example3():
     """Same as example1 but with diagonal moves available too."""
-    windSpeeds = {3:1, 4:1, 5:1, 6:2, 7:2, 8:1} 
-    wind = SouthWind(windSpeeds)
+    wind_speeds = {3:1, 4:1, 5:1, 6:2, 7:2, 8:1} 
+    wind = SouthWind(wind_speeds)
     board = Board(10, 7, wind)
-    startPos = Position(1, 3)
-    goalPos = Position(7,3)
+    start_pos = Position(1, 3)
+    goal_pos = Position(7,3)
     moves = {
         Move(1,0):"R",
         Move(-1,0):"L",
@@ -293,7 +293,7 @@ def example3():
         Move(-1,-1):"7"
         }
 
-    generateStrategy(board, startPos, goalPos, totalSteps=50000, moveSymbolMap=moves)
+    generate_strategy(board, start_pos, goal_pos, total_steps=50000, move_symbol_map=moves)
     
     
 
